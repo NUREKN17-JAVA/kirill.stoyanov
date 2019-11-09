728e7c7943ba05706ba766b7fa5fc3f7a01c65cc
Volodymyr <volodymyr.chychkanov@nure.ua>
2019-11-09 18:58:57 +0200

    The database module was written

---
+++ src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/db/HsqldbUserDao.java
@@ -0,0 +1,34 @@
+package ua.nure.kn.stoianov.usermanagement1.domain.db;
+
+import java.util.Collection;
+
+import ua.nure.kn.stoianov.usermanagement1.domain.User;
+
+public class HsqldbUserDao implements UserDao {
+
+	public User create(User user) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void update(User user) throws DatabaseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void delete(User user) throws DatabaseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public User find(Long id) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection findAll() throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
