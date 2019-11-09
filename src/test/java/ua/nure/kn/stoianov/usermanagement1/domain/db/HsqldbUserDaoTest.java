4979c15204c09ed62291b3a1b6ea2c62e13f862d
Volodymyr <volodymyr.chychkanov@nure.ua>
2019-11-09 20:40:45 +0200

    The method findAll() to search all users in the database has been implemented

--- src/test/java/ua/nure/kn/stoianov/usermanagement1/domain/db/HsqldbUserDaoTest.java
+++ src/test/java/ua/nure/kn/stoianov/usermanagement1/domain/db/HsqldbUserDaoTest.java
@@ -2,17 +2,22 @@ package ua.nure.kn.stoianov.usermanagement1.domain.db;
 
 import java.util.Date;
 
+import org.dbunit.DatabaseTestCase;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.XmlDataSet;
+
 import junit.framework.TestCase;
 import ua.nure.kn.stoianov.usermanagement1.domain.User;
 
-public class HsqldbUserDaoTest extends TestCase {
+public class HsqldbUserDaoTest extends DatabaseTestCase {
 	private HsqldbUserDao dao;
 	private ConnectionFactory connectionFactory;
 	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		connectionFactory = new ConnectionFactoryImpl();
 		dao = new HsqldbUserDao(connectionFactory);
 	}
 
@@ -31,6 +36,18 @@ public class HsqldbUserDaoTest extends TestCase {
 			fail(e.toString());
 		}
 		
+	}
+
+	@Override
+	protected IDatabaseConnection getConnection() throws Exception {
+		connectionFactory = new ConnectionFactoryImpl();
+		return new DatabaseConnection(connectionFactory.createConnection());
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		IDataSet dataSet = new XmlDataSet(getClass().getClassLoader().getResourceAsStream("usersDataSet.xml"));
+		return dataSet;
 	}
 
 }
