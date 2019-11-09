2e8301acda5eab4f2efd671f2a27bda81294235b
Kirill Stoyanow <kyrylo.stoianov@nure.ua>
2019-11-09 22:44:04 +0200

    getUserDao() method has been implemented

--- src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/db/DaoFactory.java
+++ src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/db/DaoFactory.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 
 public class DaoFactory {
 	
+	private static final String USER_DAO = "dao.ua.nure.kn.stoianov.usermanagement1.domain.db.UserDao";
 	private final Properties properties;
 	
 	public DaoFactory() {
@@ -22,6 +23,17 @@ public class DaoFactory {
 		String url = properties.getProperty("connection.url");
 		String driver = properties.getProperty("connection.driver");
 		return new ConnectionFactoryImpl(driver, url, user, password);
+	}
+	public UserDao getUserDao() {
+		UserDao result = null;
+		try {
+			Class clazz = Class.forName(properties.getProperty(USER_DAO));
+			UserDao userDao = (UserDao) clazz.newInstance();
+			userDao.setConnectionFactory(getConnectionFactory());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return result;
 	}
 }
 	
