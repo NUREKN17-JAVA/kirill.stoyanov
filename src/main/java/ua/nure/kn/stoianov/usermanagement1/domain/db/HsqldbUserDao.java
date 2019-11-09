9bbca0ae76fdb60b6358861d929d89b88286bcd0
Volodymyr <volodymyr.chychkanov@nure.ua>
2019-11-09 20:04:04 +0200

    Realization of method create() has been done

--- src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/db/HsqldbUserDao.java
+++ src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/db/HsqldbUserDao.java
@@ -1,11 +1,16 @@
 package ua.nure.kn.stoianov.usermanagement1.domain.db;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
 
 import ua.nure.kn.stoianov.usermanagement1.domain.User;
 
 public class HsqldbUserDao implements UserDao {
 	
+	private static final String INSERT_QUERY = "INSERT INTO users (firstname, lastname, dateofbirth) VALUES (?, ?, ?)";
 	private ConnectionFactory connectionFactory;
 	
 	public HsqldbUserDao(ConnectionFactory connectionFactory) {
@@ -14,8 +19,22 @@ public class HsqldbUserDao implements UserDao {
 	
 
 	public User create(User user) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Connection connection = connectionFactory.createConnection();
+			PreparedStatement statement = connection.prepareStatement(INSERT_QUERY);
+			statement.setString(1, user.getFirstName());
+			statement.setString(2, user.getLastName());
+			statement.setDate(3, new Date(user.getDateOfBirth().getTime()));
+			int n = statement.executeUpdate();
+			if (n != 1) {
+				throw new DatabaseException("Number of the inserted rows: " + n);
+			}
+			return null;
+		} catch (DatabaseException e) {
+			throw e;
+		} catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
 	}
 
 	public void update(User user) throws DatabaseException {
