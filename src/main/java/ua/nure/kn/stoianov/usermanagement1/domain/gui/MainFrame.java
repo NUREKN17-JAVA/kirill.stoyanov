5b09753b60d925e617c7cccc6daf1cdac7cbc26c
Kirill Stoyanow <kyrylo.stoianov@nure.ua>
2019-12-07 22:24:12 +0200

    MainFrameTest was created

---
+++ src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/gui/MainFrame.java
@@ -0,0 +1,12 @@
+package ua.nure.kn.stoianov.usermanagement1.domain.gui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
