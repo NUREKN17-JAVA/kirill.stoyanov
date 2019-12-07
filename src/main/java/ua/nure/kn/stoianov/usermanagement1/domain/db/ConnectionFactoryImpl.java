7338ac0a57f2d2abcb7eec81e1e0fcd02c4e6508
Kirill Stoyanow <kyrylo.stoianov@nure.ua>
2019-12-07 22:02:10 +0200

    Some changes to DaoFactory

--- src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/db/ConnectionFactoryImpl.java
+++ src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/db/ConnectionFactoryImpl.java
@@ -3,6 +3,7 @@ package ua.nure.kn.stoianov.usermanagement1.domain.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactoryImpl implements ConnectionFactory {
 
@@ -16,6 +17,13 @@ public class ConnectionFactoryImpl implements ConnectionFactory {
 		this.url = url;
 		this.user = user;
 		this.password = password;
+	}
+
+	public ConnectionFactoryImpl(Properties properties) {
+		user = properties.getProperty("connection.user");
+		password = properties.getProperty("connection.password");
+		url = properties.getProperty("connection.url");
+		driver = properties.getProperty("connection.driver");
 	}
 
 	public Connection createConnection() throws DatabaseException {
--- src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/db/DaoFactory.java
+++ src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/db/DaoFactory.java
@@ -3,34 +3,50 @@ package ua.nure.kn.stoianov.usermanagement1.domain.db;
 import java.io.IOException;
 import java.util.Properties;
 
-public class DaoFactory {
+public abstract class DaoFactory {
 	
-	private static final String USER_DAO = "dao.ua.nure.kn.stoianov.usermanagement1.domain.db.UserDao";
-	private final Properties properties;
+	protected static final String USER_DAO = "dao.ua.nure.kn.stoianov.usermanagement1.domain.db.UserDao";
+	protected static Properties properties;
 	
-	private final static DaoFactory INSTANCE = new DaoFactory();
+	private static DaoFactory instance;
+	private static final String DAO_FACTORY = "dao.factory";
 	
-	public 	static DaoFactory getInstance() {
-		return INSTANCE;
-	}
-			
-	private DaoFactory() {
+	static {
 		properties = new Properties();
 		try {
-			properties.load(getClass().getClassLoader().getResourceAsStream("settings.properties"));
+			properties.load(DaoFactory.class.getClassLoader().getResourceAsStream("settings.properties"));
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 	
-	private ConnectionFactory getConnectionFactory() {
-		String user = properties.getProperty("connection.user");
-		String password = properties.getProperty("connection.password");
-		String url = properties.getProperty("connection.url");
-		String driver = properties.getProperty("connection.driver");
-		return new ConnectionFactoryImpl(driver, url, user, password);
+	public 	static synchronized DaoFactory getInstance() {
+		if (instance == null) {
+			Class factoryClass;
+			try {
+				factoryClass = Class.forName(properties.getProperty(DAO_FACTORY));
+				instance = (DaoFactory) factoryClass.newInstance();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return instance;
 	}
-	public UserDao getUserDao() {
+			
+	protected DaoFactory() {
+	
+	}
+	
+	public static void init(Properties prop) {
+		properties = prop;
+		instance = null;
+	}
+	
+	protected ConnectionFactory getConnectionFactory() {
+		
+		return new ConnectionFactoryImpl (properties);
+	}
+	public abstract UserDao getUserDao(); {
 		UserDao result = null;
 		try {
 			Class clazz = Class.forName(properties.getProperty(USER_DAO));
@@ -39,7 +55,7 @@ public class DaoFactory {
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		return result;
+		//return result;
 	}
 }
 	
