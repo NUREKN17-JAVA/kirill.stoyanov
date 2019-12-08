ee2b1dc9b3f8e5388ab723bace8373a234e24ddb
Kirill Stoyanow <kyrylo.stoianov@nure.ua>
2019-12-08 17:46:03 +0200

    Preparing a test for the add user button in the addPanel panel

--- src/test/java/ua/nure/kn/stoianov/usermanagement1/domain/gui/MainFrameTest.java
+++ src/test/java/ua/nure/kn/stoianov/usermanagement1/domain/gui/MainFrameTest.java
@@ -1,6 +1,8 @@
 package ua.nure.kn.stoianov.usermanagement1.domain.gui;
 
 import java.awt.Component;
+import java.text.DateFormat;
+import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -10,6 +12,7 @@ import javax.swing.JTextField;
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
 import junit.extensions.jfcunit.eventdata.MouseEventData;
+import junit.extensions.jfcunit.eventdata.StringEventData;
 import junit.extensions.jfcunit.finder.NamedComponentFinder;
 
 public class MainFrameTest extends JFCTestCase {
@@ -39,28 +42,64 @@ public class MainFrameTest extends JFCTestCase {
 	}
 	
 	public void testBrowseControls() {
-		find(JPanel.class, "browsePanel");
-		JTable table = (JTable) find(JTable.class, "userTable");
+		find(JPanel.class, "browsePanel"); //$NON-NLS-1$
+		JTable table = (JTable) find(JTable.class, "userTable"); //$NON-NLS-1$
 		assertEquals(3, table.getColumnCount());
-		assertEquals("ID", table.getColumnName(0));
-		assertEquals("Имя", table.getColumnName(1));
-		assertEquals("Фамилия", table.getColumnName(2));
-		find(JButton.class, "addButton");
-		find(JButton.class, "editButton");
-		find(JButton.class, "deleteButton");
-		find(JButton.class, "detailsButton");
+		assertEquals(Messages.getString("MainFrameTest.id"), table.getColumnName(0)); //$NON-NLS-1$
+		assertEquals(Messages.getString("MainFrameTest.first_name"), table.getColumnName(1)); //$NON-NLS-1$
+		assertEquals(Messages.getString("MainFrameTest.last_name"), table.getColumnName(2)); //$NON-NLS-1$
+		find(JButton.class, "addButton"); //$NON-NLS-1$
+		find(JButton.class, "editButton"); //$NON-NLS-1$
+		find(JButton.class, "deleteButton"); //$NON-NLS-1$
+		find(JButton.class, "detailsButton"); //$NON-NLS-1$
 	}
 
 	public void testAddUser() {
-		JButton addButton = (JButton) find(JButton.class, "addButton");
+		JTable table = (JTable) find(JTable.class, "userTable");
+		assertEquals(0, table.getRowCount());
+		
+		JButton addButton = (JButton) find(JButton.class, "addButton"); //$NON-NLS-1$
 		getHelper().enterClickAndLeave(new MouseEventData(this, addButton));
 
-		find(JPanel.class, "addPanel");
+		find(JPanel.class, "addPanel"); //$NON-NLS-1$
 		
-		find(JTextField.class, "firstNameField");
-		find(JTextField.class, "lastNameField");
-		find(JTextField.class, "dateOfBirthField");
-		find(JButton.class, "okButton");
-		find(JButton.class, "cancelButton");
+		JTextField firstNameField = (JTextField) find(JTextField.class, "firstNameField"); //$NON-NLS-1$
+		JTextField lastNameField = (JTextField) find(JTextField.class, "lastNameField"); //$NON-NLS-1$
+		JTextField dateOfBirthField = (JTextField) find(JTextField.class, "dateOfBirthField"); //$NON-NLS-1$
+		JButton okButton = (JButton) find(JButton.class, "okButton"); //$NON-NLS-1$
+		find(JButton.class, "cancelButton"); //$NON-NLS-1$
+		
+		getHelper().sendString(new StringEventData(this, firstNameField, "John"));
+		getHelper().sendString(new StringEventData(this, lastNameField, "Doe"));
+		DateFormat formatter = DateFormat.getDateInstance();
+		String date = formatter.format(new Date());
+		getHelper().sendString(new StringEventData(this, dateOfBirthField, date));
+		
+		getHelper().enterClickAndLeave(new MouseEventData(this, okButton));
+		
+		find(JPanel.class, "browsePanel");
+		table = (JTable) find(JTable.class, "userTable");
+		assertEquals(1, table.getRowCount());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
