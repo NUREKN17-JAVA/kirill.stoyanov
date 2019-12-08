fa46b6cc67ff15fbaa13b3e487723dfc1b0b7308
Kirill Stoyanow <kyrylo.stoianov@nure.ua>
2019-12-08 15:33:21 +0200

    Implementation of the function of the event handler for pressing the Add button on the main form

--- src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/gui/MainFrame.java
+++ src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/gui/MainFrame.java
@@ -51,4 +51,21 @@ public class MainFrame extends JFrame {
 
 	}
 
+	public void showAddPanel() {
+		showPanel(getAddPanel());
+		
+	}
+
+	private void showPanel(JPanel panel) {
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setVisible(true);
+		panel.repaint();
+		
+	}
+
+	private AddPanel getAddPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
