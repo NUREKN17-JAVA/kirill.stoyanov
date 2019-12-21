a01641169a64793b767c397f4712fb3a1e79c2f8
--replace-all <kyrylo.stoianov@nure.ua>
2019-12-21 20:52:09 +0200

    Checking input passed to servlet

--- src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/web/EditServlet.java
+++ src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/web/EditServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.ValidationException;
 
 import ua.nure.kn.stoianov.usermanagement1.domain.User;
 import ua.nure.kn.stoianov.usermanagement1.domain.db.DaoFactory;
@@ -26,18 +27,24 @@ public class EditServlet extends HttpServlet {
 	       }
 	   }
 
-	private void showPage(HttpServletRequest req, HttpServletResponse resp) {
-		// TODO Auto-generated method stub
+	private void showPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		 req.getRequestDispatcher("/edit.jsp").forward(req, resp);
 		
 	}
 
-	private void doCancel(HttpServletRequest req, HttpServletResponse resp) {
-		// TODO Auto-generated method stub
+	private void doCancel(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/browse").forward(req, resp);
 		
 	}
 
 	private void doOk(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		User user = getUser(req);
+		User user = null;
+		try {
+			user = getUser(req);
+		} catch (ValidationException e1) {
+			req.setAttribute("error", e1.getMessage());
+			showPage(req, resp);
+		}
 		try {
 			processUser(user);
 		} catch (DatabaseException e) {
@@ -48,20 +55,33 @@ public class EditServlet extends HttpServlet {
 		
 	}
 
-	private User getUser(HttpServletRequest req) {
+	private User getUser(HttpServletRequest req) throws ValidationException {
 		User user = new User();
 		String idStr = req.getParameter("id");
 		String firstName = req.getParameter("firstName");
 		String lastName = req.getParameter("lastName");
 		String dateStr = req.getParameter("date");
 		
+		if (firstName == null) {
+			throw new ValidationException("First name is empty");
+		}
+		if (lastName == null) {
+			throw new ValidationException("Last name is empty");
+		}
+		if (dateStr == null) {
+			throw new ValidationException("Date is empty");
+		}
+		
+		
 		if (idStr != null) {
 			user.setId(new Long(idStr));
 		}
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
 		try {
 			user.setDateOfBirth(DateFormat.getDateInstance().parse(dateStr));
 		} catch (ParseException e) {
-			throw new RuntimeException(e);
+			throw new ValidationException("Date format is incorrect");
 		}
 		return user;
 	}
--- src/test/java/ua/nure/kn/stoianov/usermanagement1/domain/web/EditServletTest.java
+++ src/test/java/ua/nure/kn/stoianov/usermanagement1/domain/web/EditServletTest.java
@@ -22,4 +22,36 @@ public class EditServletTest extends MockServletTestCase {
         addRequestParameter("okButton", "Ok");
         doPost();
     }
+	
+	public void testEditEmptyFirstName() {
+        User user = new User(new Long(1000), "John", "Doe", new Date());
+        addRequestParameter("id", user.getId().toString());
+        addRequestParameter("lastName", user.getLastName());
+        addRequestParameter("date", DateFormat.getDateInstance().format(user.getDateOfBirth()));
+        addRequestParameter("okButton", "Ok");
+        doPost();
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute("error");
+        assertNotNull("Could not find error message in session scope", errorMessage);
+    }
+	public void testEditEmptyLastName() {
+        User user = new User(new Long(1000), "John", "Doe", new Date());
+        addRequestParameter("id", user.getId().toString());
+        addRequestParameter("firstName", user.getFirstName());
+        addRequestParameter("dateOfBirthd", DateFormat.getDateInstance().format(user.getDateOfBirth()));
+        addRequestParameter("ok", "Ok");
+        doPost();
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute("error");
+        assertNotNull("Could not find error message in session scope", errorMessage);
+    }
+	public void testEditEmptyDate() {
+        User user = new User(new Long(1000), "John", "Doe", new Date());
+        addRequestParameter("id", user.getId().toString());
+        addRequestParameter("firstName", user.getFirstName());
+        addRequestParameter("lastName", user.getLastName());
+        addRequestParameter("dateOfBirthd", "bla-bla-bla");
+        addRequestParameter("ok", "Ok");
+        doPost();
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute("error");
+        assertNotNull("Could not find error message in session scope", errorMessage);
+    }
 }
