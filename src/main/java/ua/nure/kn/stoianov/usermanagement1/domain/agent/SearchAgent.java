8f5fdfdd56db9e2f0205c10b7d53c91f86f3e9c0
--replace-all <kyrylo.stoianov@nure.ua>
2019-12-22 14:05:57 +0200

    Getting a list of agents providing a search service

--- src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/agent/SearchAgent.java
+++ src/main/java/ua/nure/kn/stoianov/usermanagement1/domain/agent/SearchAgent.java
@@ -1,36 +1,83 @@
 package ua.nure.kn.stoianov.usermanagement1.domain.agent;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import ua.nure.kn.stoianov.usermanagement1.domain.db.DaoFactory;
 import ua.nure.kn.stoianov.usermanagement1.domain.db.DatabaseException;
 
 public class SearchAgent extends Agent {
 
-	@Override
+	private AID[] aids;
+	
 	protected void setup() {
 		super.setup();
 		System.out.print(getAID().getName() + " sarted");
+		
+		DFAgentDescription description = new DFAgentDescription();
+		description.setName(getAID());
+		ServiceDescription serviceDescription = new ServiceDescription();
+		serviceDescription.setName("JADE-searching");
+		serviceDescription.setType("searching");
+		description.addServices(serviceDescription);
+		try {
+			DFService.register(this, description);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+		
+		
+		addBehaviour(new TickerBehaviour(this, 60000) {
+
+			protected void onTick() {
+				DFAgentDescription agentDescription = new DFAgentDescription();
+				ServiceDescription serviceDescription = new ServiceDescription();
+				serviceDescription.setType("searching");
+				agentDescription.addServices(serviceDescription);
+				try {
+					DFAgentDescription[] description = DFService.search(myAgent, agentDescription);
+					aids = new AID[description.length];
+					for (int i = 0; i < description.length; i++) {
+						DFAgentDescription d = description[i];
+						aids[i] = d.getName();
+					}
+				} catch (FIPAException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		addBehaviour(new RequestServer());
 	}
 
 	@Override
 	protected void takeDown() {
 		System.out.println(getAID().getName() + " terminated");
+		try {
+			DFService.deregister(this);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
 		super.takeDown();
 	}
 	
-	public void search(String firstName, String lastName) throws SearchException {
+	public void search(String firstName, String lastName) throws SQLException {
 		try {
 			Collection users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
 			if (users.size() > 0) {
 				showUsers(users);
 			} else {
-				addBehaviour(new SearchRequestBehaviour(new AID[] {}, firstName, lastName));
+				addBehaviour(new SearchRequestBehaviour(aids, firstName, lastName));
 			}
 		} catch (DatabaseException e) {
-			throw new SearchException(e);
+			throw new SQLException(e);
 		}
 
 	}
